38325a7f823ad8d6e6843163e9f9d3479a7ae26a
Mohini Khare <mohinilahanukhare@gmail.com>
2026-09-19 21:22:11 +0530

    Render : Deep Search

--- 2_openai/deep_research/app.py
+++ 2_openai/deep_research/app.py
@@ -2,6 +2,7 @@ import gradio as gr
 from dotenv import load_dotenv
 from research_manager import ResearchManager
 from styles import CSS, JS, EXAMPLES, HEADER_HTML
+import os
 
 load_dotenv(override=True)
 
@@ -35,4 +36,10 @@ with gr.Blocks(title="Deep Research") as ui:
 
 
 if __name__ == "__main__":
-    ui.launch(css=CSS, js=JS, theme=gr.themes.Base())
+    ui.launch(
+        css=CSS,
+        js=JS,
+        theme=gr.themes.Base(),
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860))
+    )
